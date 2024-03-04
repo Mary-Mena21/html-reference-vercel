36c88004460c648dffc5d25e924902b251f6bf60
Mariana Mena <maranashokry@ahoo.com>
2024-03-04 09:20:32 -0600

    test

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ document.querySelector('#app').innerHTML = `
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
-    <p>testtttttttttttttttttt</p>
+    <p>testtttttttttttttttttt2</p>
   </div>
 `
 
